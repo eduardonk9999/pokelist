9c95569aa868a1608f310abc7967c608821b0d08
eduardonk9999 <thiago.novak7@gmail.com>
2021-08-16 11:46:48 -0300

    Add, axios, renderizei elementos na tela, já estamos buscando pokemons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Home from "./pages/Home";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>PokeList</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+export default function Home(){
+  //para a busca const [pokemonName, setPokemonsNames] = useState("pikachu")
+  const [pokemons, setPomemons ] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=10&offset=10')
+      .then(response => { setPomemons(response.data.results) })
+
+
+  
+  }, [])
+
+  return(
+    <>
+      {pokemons.map(pokemon => (
+        <li key={pokemon.name}>{pokemon.name}</li>
+      ))}
+    </>
+  )
+}
